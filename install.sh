a0574b56f433a34f5228f223009e984a821365a4
Dave Haeffner <dave@arrgyle.com>
2013-08-23 00:36:44 -0400

    Added folder structure and updated the install script

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-ln -s .vimrc ~/.vimrc
+#!/bin/bash
+
+#Initialize submodules
+git submodule init; git submodule update;
+
+curr_dir=`pwd`
+
+ln -s $curr_dir/vim $HOME/.vim
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
 
 touch $HOME/.baseline_dotfiles
