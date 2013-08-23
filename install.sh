4094b7ebefb67db80cfa04a39b54f862a16fe6c1
Dave Haeffner <dave@arrgyle.com>
2013-08-23 17:05:04 -0400

    Added bash support and installation

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ curr_dir=`pwd`
 ln -s $curr_dir/vim ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 
+ln -s $curr_dir/bash/bashrc ~/.bashrc
+ln -s $curr_dir/bash/bash_profile ~/.bash_profile
+
 touch ~/.baseline_dotfiles
