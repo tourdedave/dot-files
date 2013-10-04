4161141b7106ca2dd113f27892a4417900e97c6f
vagrant <vagrant@dots-ruby193>
2013-10-04 03:28:31 +0000

    Changed the file cleanup on install from moving what's there to create a backup to just remove instead

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ git submodule init; git submodule update;
 
 curr_dir=`pwd`
 
-[[ -d $HOME/.vim ]] && mv $HOME/.vim $HOME/.vim.backup
+rm -rf $HOME/.vim
 ln -s $curr_dir/vim $HOME/.vim
 
-[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc.backup
+rm $HOME/.vimrc
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 
-[[ -f $HOME/.bashrc ]] && mv $HOME/.bashrc $HOME/.bashrc.backup
+rm $HOME/.bashrc
 ln -s $curr_dir/bash/bashrc $HOME/.bashrc
 
-[[ -f $HOME/.bash_profile ]] && mv $HOME/.bash_profile $HOME/.bash_profile.backup
+rm $HOME/.bash_profile
 ln -s $curr_dir/bash/bash_profile $HOME/.bash_profile
 
-[[ -f $HOME/.irbrc ]] && mv $HOME/.irbrc $HOME/.irbrc.backup
+rm $HOME/.irbrc
 ln -s $curr_dir/irbrc $HOME/.irbrc
 
-[[ -d $HOME/.rbenv ]] && mv $HOME/.rbenv $HOME/.rbenv.backup
+rm $HOME/.rbenv
 ln -s $curr_dir/rbenv $HOME/.rbenv
 
 touch $HOME/.baseline_dotfiles
